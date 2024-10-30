5375bdcb8d4e275414672a7acf60c01f992bec04
vasudevbkn2005 <vasudevsarswatbkn@gmail.com>
2024-10-30 15:45:40 +0530

    Project Section 1

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,4 @@
+<footer>
+    <div class="footer-content text-center">
+        <p>&copy; 2024 Your Company Name. All rights reserved.</p>
+</footer>
---
+++ resources/views/master.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ecart</title>
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
+        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
+<style>
+    .custom-login{
+        height: 650px;
+        padding-top:100px; 
+    }
+</style>
+</html>
